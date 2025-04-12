b9e6940a0d1732ad3eb77abcac16030ee03c352f
Seth Oberlitner <oberse@proton.me>
2025-04-11 23:12:15 -0400

    get angular html working

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -14,7 +14,8 @@ return {
       },
     },
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
+      notify_no_formatters = true,
       format_on_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
@@ -35,10 +36,11 @@ return {
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        html = { 'prettierd', 'prettier', stop_after_first = true },
-        css = { 'prettierd', 'prettier', stop_after_first = true },
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        htmlangular = { 'prettierd' },
+        html = { 'prettierd' },
+        css = { 'prettierd' },
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
       },
     },
   },
